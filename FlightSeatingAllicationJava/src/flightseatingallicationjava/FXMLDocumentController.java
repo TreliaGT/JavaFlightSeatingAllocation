0a8860ebac05d45cbad726b994174a7e63f5e8a7
TreliaGT <v244682@tafe.wa.edu.au>
2019-05-27 14:36:53 +0800

    added more to the gui & array

--- FlightSeatingAllicationJava/src/flightseatingallicationjava/FXMLDocumentController.java
+++ FlightSeatingAllicationJava/src/flightseatingallicationjava/FXMLDocumentController.java
@@ -7,12 +7,15 @@ package flightseatingallicationjava;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
@@ -23,7 +26,6 @@ import javafx.scene.control.TextField;
 public class FXMLDocumentController implements Initializable {
     
    
-    String plane[][] = new String[12][6]; 
     @FXML
     private TableView<?> FlightSeatsGrid;
     @FXML
@@ -34,15 +36,35 @@ public class FXMLDocumentController implements Initializable {
     private ComboBox<?> ClassComboBox;
     @FXML
     private ComboBox<?> SeatTypeCombo;
-   
-    
+ 
+      String plane[][] ={
+        {"Row 1", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 2", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 3", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 4", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 5", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 6", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 7", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 8", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 9", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 10", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 11", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+          {"Row 12", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
+    };
+      
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        ObservableList row = FXCollections.observableArrayList(plane);
+
+            FlightSeatsGrid.setItems(row);
+      
+    
     }    
 
     @FXML
     private void Close_Click(ActionEvent event) {
+        
     }
 
     @FXML
