794772ece6d799afc4b04e9a60ad5489d6f97330
TreliaGT <v244682@tafe.wa.edu.au>
2019-05-31 16:35:26 +0800

    tried tableview

--- FlightSeatingAllicationJava/src/flightseatingallicationjava/FXMLDocumentController.java
+++ FlightSeatingAllicationJava/src/flightseatingallicationjava/FXMLDocumentController.java
@@ -6,19 +6,19 @@
 package flightseatingallicationjava;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TreeTableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  *
@@ -32,14 +32,27 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TextField txtName;
     @FXML
-    private ComboBox<?> AgeComboBox;
+    private ComboBox<String> AgeComboBox;
     @FXML
-    private ComboBox<?> ClassComboBox;
+    private ComboBox<String> ClassComboBox;
     @FXML
-    private ComboBox<?> SeatTypeCombo;
-    private TableView<?> tableView;
-    
-         String plane[][] ={
+    private ComboBox<String> SeatTypeCombo;
+    private TableView<Seating> tableView;
+    @FXML
+    private TableColumn<Seating, String> rows;
+    @FXML
+    private TableColumn<Seating, String> A;
+    @FXML
+    private TableColumn<Seating, String> B;
+    @FXML
+    private TableColumn<Seating, String> C;
+    @FXML
+    private TableColumn<Seating, String> D;
+    @FXML
+    private TableColumn<Seating, String> E;
+    @FXML
+    private TableColumn<Seating, String> F;
+    String plane[][] ={
         {"Row 1", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
           {"Row 2", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
           {"Row 3", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
@@ -53,34 +66,33 @@ public class FXMLDocumentController implements Initializable {
           {"Row 11", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
           {"Row 12", "*" ,"*" ,"*" ,"*" ,"*" , "*"},
     };
-    @FXML
-    private TableColumn<?, ?> rows;
-    @FXML
-    private TableColumn<?, ?> A;
-    @FXML
-    private TableColumn<?, ?> B;
-    @FXML
-    private TableColumn<?, ?> C;
-    @FXML
-    private TableColumn<?, ?> D;
-    @FXML
-    private TableColumn<?, ?> E;
-    @FXML
-    private TableColumn<?, ?> F;
 
    
          
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        ObservableList row = FXCollections.observableArrayList(plane);
+        ArrayList<Seating> list = new ArrayList<>();
 
-            tableView.setItems(row);
+       for(int i = 0; i > plane.length; i ++){
+           
+           list.add(new Seating(plane[i][0],plane[i][1], plane[i][2],plane[i][3],plane[i][4],plane[i][5],plane[i][6]));
+       }
+       ObservableList<Seating> list2 = FXCollections.observableArrayList(list);                 
+        tableView.getColumns().addAll(rows, A, B,C,D,E,F);        
+        rows.setCellValueFactory(data -> data.getValue().PropertyRows());
+        A.setCellValueFactory(data -> data.getValue().PropertyA());
+        B.setCellValueFactory(data -> data.getValue().PropertyB()); 
+        C.setCellValueFactory(data -> data.getValue().PropertyC()); 
+        D.setCellValueFactory(data -> data.getValue().PropertyD()); 
+        E.setCellValueFactory(data -> data.getValue().PropertyE()); 
+        F.setCellValueFactory(data -> data.getValue().PropertyF()); 
+        tableView.setItems(list2);
     }    
 
     @FXML
     private void Close_Click(ActionEvent event) {
-        
+    
     }
 
     @FXML
---
+++ FlightSeatingAllicationJava/src/flightseatingallicationjava/Seating.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightseatingallicationjava;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author V244682
+ */
+public class Seating {
+    private SimpleStringProperty rows;
+    private SimpleStringProperty A;
+    private SimpleStringProperty B;
+    private SimpleStringProperty C;
+    private SimpleStringProperty D;
+    private SimpleStringProperty E;
+    private SimpleStringProperty F;
+
+    public Seating(String rows, String A, String B, String C, String D, String E, String F) {
+         this.rows.set(rows);
+         this.A.set(A);
+         this.B.set(B);
+         this.C.set(C);
+         this.D.set(D);
+         this.E.set(E);
+         this.F.set(F);
+    }
+    
+    public SimpleStringProperty PropertyRows() {
+        return rows;
+    }
+
+    public void setRows(String rows) {
+         this.rows.set(rows);
+    }
+    
+    public String getRows() {
+        return rows.get();
+    }
+    
+    public SimpleStringProperty PropertyA() {
+        return A;
+    }
+
+    public void setA(String A) {
+        this.A.set(A);
+    }
+    
+     public String getA() {
+        return A.get();
+    }
+    
+
+    public SimpleStringProperty PropertyB() {
+        return B;
+    }
+
+    public void setB(String B) {
+        this.B.set(B);
+    }
+    
+      public String getB() {
+        return B.get();
+    }
+
+    public SimpleStringProperty PropertyC() {
+        return C;
+    }
+
+    public void setC(String C) {
+        this.C.set(C);
+    }
+    
+      public String getC() {
+        return C.get();
+    }
+    public SimpleStringProperty PropertyD() {
+        return D;
+    }
+
+    public void setD(String D) {
+        this.D.set(D);
+    }
+    
+    public String getD() {
+        return D.get();
+    }
+    public SimpleStringProperty PropertyE() {
+        return E;
+    }
+
+    public void setE(String E) {
+        this.E.set(E);
+    }
+
+    public String getE() {
+        return E.get();
+    }
+    public SimpleStringProperty PropertyF() {
+        return F;
+    }
+
+    public void setF(String F) {
+        this.F.set(F);
+    }
+
+        public String getF() {
+        return F.get();
+    }
+    
+                         
+}
